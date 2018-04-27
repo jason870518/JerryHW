69662ee1784a476b8b3f91c1e50125735ad532dd
jason870518 <cxz010511@gmail.com>
2018-04-27 22:56:02 +0800

    0416
    0416

--- 0416/jason_django/mysite/jason/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JasonConfig(AppConfig):
-    name = 'jason'
--- 0416/jason_django/mysite/jason/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
-from .models import Introduction
-# Create your views here.
-def index(request):
-	introdutions = Introduction.objects.all()
-	return render_to_response('jason/menu.html', locals())
