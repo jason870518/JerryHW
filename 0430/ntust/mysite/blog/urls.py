ae820d8460318381dc70169176989715deec9de1
jason870518 <cxz010511@gmail.com>
2018-05-11 23:05:52 +0800

    0430
    0430

--- 0430/ntust/mysite/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views 
-
-urlpatterns = [
-    url(r'^$', views.blog, name='blog'),
-    url(r'^new/', views.new, name="new"),
-    url(r'^edit/', views.edit, name="edit"),
-]
--- 0430/ntust/mysite/mothers_day/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PollsConfig(AppConfig):
-    name = 'mothers_day'
